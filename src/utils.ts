015b3896a703910d4445b7368ff71fee78d34df8
Guilherme Oliveira <guilherme.oliveira@luizalabs.com>
2021-08-14 14:48:16 -0300

    Adding array length check

--- src/utils.ts
+++ src/utils.ts
@@ -7,22 +7,24 @@ export const getIDs = async (
 	itemService: ExtensionContext['services']['ItemsService'],
 	schema: EventHandlerArguments['schema']
 ) => {
-	let ids: string[] = [];
+	if (values.length > 0) {
+		let ids: string[] = [];
 
-	const relationsIDs = values.filter((el) => typeof el === 'number');
-	const propositionsIDs = values.filter((el) => typeof el === 'object').map((el) => el.proposicoes_id);
+		const relationsIDs = values.filter((el) => typeof el === 'number');
+		const propositionsIDs = values.filter((el) => typeof el === 'object').map((el) => el.proposicoes_id);
 
-	const relationService = new itemService(
-		`${type === 'single' ? 'ordem_do_dia_proposicoes' : 'ordem_do_dia_proposicoes_1'}`,
-		{ schema }
-	);
+		const relationService = new itemService(
+			`${type === 'single' ? 'ordem_do_dia_proposicoes' : 'ordem_do_dia_proposicoes_1'}`,
+			{ schema }
+		);
 
-	const relationsData = await relationService.readMany(relationsIDs);
-	relationsData.map((relation) => ids.push(relation.proposicoes_id));
+		const relationsData = await relationService.readMany(relationsIDs);
+		relationsData.map((relation) => ids.push(relation.proposicoes_id));
 
-	ids = [...ids, ...propositionsIDs];
+		ids = [...ids, ...propositionsIDs];
 
-	return ids;
+		return ids;
+	} else return [];
 };
 
 export const getArrayIntersection = (array1: any[], array2: any[]) => {
